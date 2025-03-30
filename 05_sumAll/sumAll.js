c5945edddad7d86f9151f73bb267d4d1dfe3b4f3
Haezed <130173952+Haezed@users.noreply.github.com>
2025-03-30 02:42:38 -0400

    completed excersize 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    let finalSum = 0;
+
+    for (i = min; i <= max; i++) {
+        finalSum += 1;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
