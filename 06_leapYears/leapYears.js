9453f23f2e807fad703af16c2d875076c3c3c755
Haezed <130173952+Haezed@users.noreply.github.com>
2025-03-30 03:01:10 -0400

    completed excersize 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const divisibleByOneHundred = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
 
+    if (divisibleByFour && 
+      (!divisibleByOneHundred || divisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
